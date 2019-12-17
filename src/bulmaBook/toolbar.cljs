906d7208464c42e68ae3d11b7b28412b3c1980e0
Tim Cross <theophilusx@gmail.com>
2019-12-17 15:39:34 +1100

    Add toolbar component

---
+++ src/bulmaBook/toolbar.cljs
@@ -0,0 +1,23 @@
+(ns bulmaBook.toolbar
+  (:require [bulmaBook.utils :as utils]))
+
+(defn defitem [& {:keys [type class content]
+                  :or {type :div}}]
+  {:type type
+   :class class
+   :content content})
+
+(defn toolbar [& {:keys [left-items right-items class]}]
+  [:nav {:class (utils/cs "level" class)}
+   (into
+    [:div.level-left]
+    (for [i left-items]
+      [(:type i) {:class (utils/cs "level-item" (:class i))}
+       (:content i)]))
+   (when right-items
+     (into
+      [:div.level-right]
+      (for [i right-items]
+        [(:type i) {:class (utils/cs "level-item" (:class i))}
+         (:content i)])))])
+
