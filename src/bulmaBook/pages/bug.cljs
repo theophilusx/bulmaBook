09be5886dd4e5181fd692a50e2e67f5ce65f0513
Tim Cross <theophilusx@gmail.com>
2020-02-19 14:02:01 +1100

    Added bug reporting page

---
+++ src/bulmaBook/pages/bug.cljs
@@ -0,0 +1,35 @@
+(ns bulmaBook.pages.bug
+  (:require [reagent.session :as session]
+            [bulmaBook.utils :refer [session-path]]
+            [bulmaBook.components.form :as form]
+            [bulmaBook.data :as data]))
+
+(defn do-report []
+  (let [idx (count (session/get-in [:data :bug-reports]))]
+    (session/assoc-in! [:data :bug-reports (keyword (str "bug-" idx))]
+                       {:subject (session/get-in [:bug :subject])
+                        :expected (session/get-in [:bug :expected])
+                        :actual (session/get-in [:bug :actual])
+                        :reproduce (session/get-in [:bug :reproduce])})
+    (session/remove! :bug)
+    (session/assoc-in! (session-path data/navbar-id) :home)))
+
+(defn bug-report []
+  [:section
+   [:div.container
+    [:div.columns.is-centered
+     [:div.column.is-7-tablet.is-6-desktop.is-5-widescreen
+      [:h2.title.is-2 "Bug Report"]
+      [:p (str "When making a bug report, please include the following;")]
+      [:div.content
+       [:ul
+        [:li "Platform and browser version"]
+        [:li "Minimal set of steps to follow to reproduce the issue"]
+        [:li "Any special or specific configuration used"]]]
+      [form/horizontal-field "Subject"
+       [[form/input :text nil :bug.subject]]]
+      [form/horizontal-field "Expected Behaviour"
+       [[form/textarea nil :bug.expected :placeholder "What did you expect to happen?"]]]
+      [form/horizontal-field "Actual Behaviour"
+       [[form/textarea nil :bug.actual :placeholder "What did you observe?"]]]
+      [form/button "Submit" do-report :button-class "is-primary"]]]]])
