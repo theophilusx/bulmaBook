9f9adfb769d38693ac72c507ea30ce72a21138ff
Tim Cross <theophilusx@gmail.com>
2020-03-01 13:28:58 +1100

    document form components

--- src/bulmaBook/components/form.cljs
+++ src/bulmaBook/components/form.cljs
@@ -4,7 +4,13 @@
             [reagent.session :as session]
             [reagent.core :as r]))
 
-(defn field [body & {:keys [label field-class label-class]}]
+(defn field
+  "A generic field component. The `body` argument is a vector of form control
+  components. Available optional keyword arguments include
+  `label` - a field label
+  `field-class` - additional classes to add to the `field` element
+  `label-class` - additional classes to add to the label element"
+  [body & {:keys [label field-class label-class]}]
   (into
    [:div {:class (cs "field" field-class)}
     (when label
@@ -12,7 +18,16 @@
    (for [el body]
      el)))
 
-(defn horizontal-field [label body & {:keys [field-class label-class body-class]}]
+(defn horizontal-field
+  "A component for the layout of horizontal fields in a form. The `label`
+  argument becomes the label for the fields. The `body` argument is a vector
+  of form control element components e.g. input element. Additional optional
+  keyword arguments include
+  `field-class` - additional classes to add to the field element
+  `label-class` - additional classes to add to the label element
+  `body-class` - additional classes to add to the `field-body` element"
+  [label body & {:keys [field-class label-class
+                        body-class]}]
   [:div {:class (cs "field" "is-horizontal" field-class)}
    [:div {:class (cs "field-label" field-class)}
     [:label {:class (cs "label" label-class)}
@@ -33,8 +48,21 @@
            :required required
            :on-change #(session/assoc-in! (session-path id) (value-of %))}])
 
-(defn input [type id & {:keys [control-class input-class placeholder required
-                               icon]}]
+(defn input
+  "A basic `input` component. The `type` argument is a keyword representing the
+  input type. Supported types are the HTML input types as keywords. The `id`
+  argument is a keyword that defines the path into the session object used to
+  store the value entered in the input component. The `id` keyword uses a
+  period `.` as a path separator e.g. :person.first-name will be translated into
+  `[:person :first-name]. Additional optional keyword arguments include
+  `control-class` - additional classes to add to the `control` element
+  `input-class` - additional classes to add to the `input` element
+  `placeholder` - placeholder text to place in the `input` element
+  `required` - if true, add the required attribute to the `input` element
+  `icon` - either an `icon` map object (see `bulmaBook.components.basic`) or a
+           vector of `icon` map objects"
+  [type id & {:keys [control-class input-class placeholder required
+                     icon]}]
   (if icon
     (into
      [:div {:class (cs "control" control-class
@@ -46,15 +74,37 @@
     [:div {:class (cs "control" control-class)}
      (input-helper type id input-class placeholder required)]))
 
-(defn input-field [label type id & {:keys [field-class label-class control-class
-                                           input-class placeholder required
-                                           value icon]}]
+(defn input-field
+  "A basic input field component. The `label` argument will be the label
+  associated with the field. The `type` argument is a keyword specifying the
+  input filed type. The `id` is a keyword representing a path into the global
+  session object. Any period `.` in the keyword is interpreted as a path
+  separator e.g. :a.b.c will be translated into [:a :b :c]. Additional supported
+  optional keyword arguments include
+  `field-class` - additional classes to add tot he `field` element
+  `label-class` - additional classes to be added to the `label` element
+  `control-class` - additional classes to be added to the `control` element
+  `input-class` - additional classes to be added tot he `input` element
+  `placeholder` - placeholder text to be used in the `input` element
+  `required` - if true, the `input` element has the required attribute added
+  `icon` - either an `icon` data map (see `bulmaBook.components.basic`) or a
+           vector of icon data maps"
+  [label type id & {:keys [field-class label-class control-class
+                           input-class placeholder required icon]}]
   (field [[input type id :control-class control-class :input-class input-class
-           :placeholder placeholder :required required :value value
-           :icon icon]]
+           :placeholder placeholder :required required :icon icon]]
          :label label :field-class field-class :label-class label-class))
 
-(defn checkbox [label id & {:keys [field-class label-class control-class]}]
+(defn checkbox
+  "A basic checkbox component. The `label` argument represents the text label
+  displayed after the checkbox. The `id` argument is a keyword representing
+  the path into the global session object where the value will be stored.
+  The `id` keyword uses a period `.` as a path separator e.g. :a.b.c becomes
+  [:a :b :c]. Additional optional keyword arguments are
+  `field-class` - additional classes to add to the `field` element
+  `label-class` - additional classes to add to the `label` element
+  `control-class` - additional classes to add to the `control` element"
+  [label id & {:keys [field-class label-class control-class]}]
   (let [path (session-path id)]
     [:div {:class (cs "field" field-class)}
      [:div {:class (cs "control" control-class)}
@@ -66,7 +116,14 @@
                              (session/update-in! path not))}]
        (str " " label)]]]))
 
-(defn button [title action & {:keys [field-class control-class button-class]}]
+(defn button
+  "A basic button component. The `title` argument is the text that will be
+  displayed on the button. The `action` argument is a function that will be
+  executed when the buton is clicked on. Additional optional keyword args are
+  `field-class` - additional classes to add to the `field` element
+  `control-class` - additional classes to add to the `control` element
+  `button-class` - additional classes to add to the `button` element"
+  [title action & {:keys [field-class control-class button-class]}]
   [:div {:class (cs "field" field-class)}
    [:div {:class (cs "control" control-class)}
     [:button {:class (cs "button" button-class)
@@ -82,8 +139,20 @@
   (swap! s assoc :value (session/get-in (session-path (:session-id @s))))
   (swap! s assoc :editing false))
 
-(defn editable-field [label session-id type & {:keys [field-class label-class
-                                               control-class input-class]}]
+(defn editable-field
+  "An editable field component. This component will display the value with a
+  click handler event such that when the user clicks on the value, the field
+  is replaced with an editable input field. The `label` argument will be used
+  as the field label. The `session-id` argument is a keyword path specifier
+  which identifies the path into the global session object where the value will
+  be edited. The keyword uses a period `.` as a path separator e.g. `:a.b.c`
+  will become `[:a :b :c]`. Additional optional keyword arguments include
+  `field-class` - additional classes to add to the `field` element
+  `label-class` - additional classes to add to the `label` element
+  `control-class` - additional classes to add to the `control` element
+  `input-class` - additional class elements to add to the `input` element."
+  [label session-id type & {:keys [field-class label-class
+                                   control-class input-class]}]
   (let [state (r/atom {:value (session/get-in (session-path session-id))
                        :session-id session-id
                        :editing false})]
@@ -114,8 +183,19 @@
                   :on-click #(swap! state assoc :editing true)}
            [:i {:class (cs "fa" "fa-pencil")}]]]]))))
 
-(defn textarea [label session-id & {:keys [field-class control-class label-class
-                                          textarea-class placeholder]}]
+(defn textarea
+  "A textarea component. The `label` argument specifies the label to be
+  associated with the textarea. The `session-id` argument is a keyword that
+  specifies the path into the global state atom where the textarea data will
+  be saved. The keyword uses a period `.` as a path separator e.g. `:a.b.c` will
+  become `[:a :b :c]`. Additional optional keyword arguments include
+  `field-class` - additional classes to add to the `field` element
+  `control-class` - additional classes to add to the `control` element
+  `label-class` - additional classes to add to the `label` element
+  `textarea-class` - additional classes to add to the `textarea` element
+  `placeholder` - placeholder text to add tot he `textarea` element."
+  [label session-id & {:keys [field-class control-class label-class
+                              textarea-class placeholder]}]
   [:div {:class (cs "field" field-class)}
    (when label
      [:label {:class (cs "label" label-class)} label])
@@ -127,7 +207,15 @@
                 :on-change #(session/assoc-in!
                              (session-path session-id) (value-of %))}]]])
 
-(defn option [title val & {:keys [option-class disabled selected label]}]
+(defn option
+  "A basic `option` component. The `title` argument used as the label for the
+  option. The `val` argument is the value associated with the option. Additional
+  optional keyword arguments include
+  `option-class` - additional classes added to the `option` element
+  `disabled` - true if the option is disabled
+  `selected` - true if the option is selected
+  `label` - value for the `option` label attribute"
+  [title val & {:keys [option-class disabled selected label]}]
   [:option {:class option-class
             :disabled disabled
             :selected selected
@@ -135,8 +223,21 @@
             :value val}
    title])
 
-(defn select [id options & {:keys [select-class multiple rounded select-size
-                                  icon]}]
+(defn select
+  "A `seledt` component. The `id` argument is a keyword specifying the path into
+  the global session atom. The keyword uses a period `.` as a path separator.
+  e.g. `:a.b.c` becomes `[:a :b :c]`. The `options` argument is a vector of
+  `option` components representing the options presented by the `select`
+  component. Additional optional keyword arguments include
+  `select-class` - additional classes to add to the `select` element
+  `multiple` - true if the `select` element allows multiple option selection
+  `rounded` - true if the select styling should use rounded corners
+  `select-size` - number of items to display in the select dropdown
+  `icon` - either an icon data map (see `bulmaBook.components.basic`) or a
+           vector of icon data maps which specify icons to add to the select
+           component."
+  [id options & {:keys [select-class multiple rounded select-size
+                        icon]}]
   [:div {:class (cs "select" select-class
                     (when rounded "is-rounded")
                     (when select-size
@@ -151,13 +252,29 @@
               :name (name id)
               :multiple (when multiple true false)
               :size multiple
-              :on-change #(session/assoc-in! (session-path id) (keyword (value-of %)))}]
+              :on-change #(session/assoc-in! (session-path id)
+                                             (keyword (value-of %)))}]
     (for [o options]
       o))
    [basic/icon-component icon]])
 
-(defn select-field [id options & {:keys [title field-class select-class multiple
-                                         rounded select-size icon]}]
+(defn select-field
+  "A select field component. Wraps a `select` component inside a `field`
+  and `control` component. The `id` argument is a keyword which specifies the
+  path into the global session atom where the value for the `select` component
+  will be stored. The keyword uses a period `.` as a path separator e.g. `:a.b.c`
+  will become `[:a :b :c]`. The `options` argument is a vector of `option`
+  components. Additional optional keyword arguments include
+  `title` - a title label to be associated with the `select` components
+  `field-class` - additional classes to add to the `field` components
+  `select-class` - additional classes to add to the `select` components
+  `multiple` - true if the `select` component should support multiple options
+  `rounded` - if true, `select` component will be styled with rounded corners
+  `select-size` - number of select `options` to display
+  `icon` - either an icon data map (see `bulmaBook.components.basic`) or a
+           vector of icon data maps"
+  [id options & {:keys [title field-class select-class multiple
+                        rounded select-size icon]}]
   [:div {:class (cs "field" field-class)}
    (when title
      [:div {:class "label"} title])
