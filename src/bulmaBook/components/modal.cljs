4de33a95828a1ebdf98c2a71fcd4348a09f10703
Tim Cross <theophilusx@gmail.com>
2020-03-05 10:48:09 +1100

    cleanup of modal component code

--- src/bulmaBook/components/modal.cljs
+++ src/bulmaBook/components/modal.cljs
@@ -1,44 +1,45 @@
 (ns bulmaBook.components.modal
-  (:require [bulmaBook.utils :refer [cs session-path]]
+  (:require [bulmaBook.utils :refer [session-path]]
             [reagent.session :as session]))
 
-(defn modal [body id & {:keys [modal-class background-class content-class close-class]}]
-  [:div {:class (cs "modal" modal-class
-                    (when (session/get-in (session-path id))
-                      "is-active"))}
-   [:div {:class (cs "modal-background" background-class)
-          :on-click #(session/assoc-in! (session-path id) false)}]
+(defn modal [body id & {:keys [modal-class background-class content-class
+                               close-class]}]
+  [:div.modal {:class [modal-class
+                       (when (session/get-in (session-path id))
+                         "is-active")]}
+   [:div.modal-background {:class background-class
+                           :on-click #(session/assoc-in! (session-path id) false)}]
    (into
-    [:div {:class (cs "modal-content" content-class)}]
+    [:div.modal-content {:class content-class}]
     (for [c body]
       c))
-   [:button {:class (cs "modal-close" "is-large" close-class)
-             :aria-label "close"
-             :on-click #(session/assoc-in! (session-path id) false)}]])
+   [:button.modal-close.is-large {:class close-class
+                                  :aria-label "close"
+                                  :on-click #(session/assoc-in! (session-path id) false)}]])
 
 (defn modal-card [body id & {:keys [modal-class background-class card-class
                                     header header-class body-class
                                     footer footer-class close-class]}]
-  [:div {:class (cs "modal" modal-class
-                    (when (session/get-in (session-path id))
-                      "is-active"))}
-   [:div {:class (cs "modal-background" background-class)
-          :on-click #(session/assoc-in! (session-path id) false)}]
-   [:div {:class (cs "modal-card" card-class)}
+  [:div.modal {:class [modal-class
+                       (when (session/get-in (session-path id))
+                         "is-active")]}
+   [:div.modal-background {:class background-class
+                           :on-click #(session/assoc-in! (session-path id) false)}]
+   [:div.modal-card {:class card-class}
     (when header
       (into
-       [:header {:class (cs "modal-card-head" header-class)}]
+       [:header.modal-card-head {:class header-class}]
        (for [h header]
          h)))
     (into
-     [:section {:class (cs "modal-card-body" body-class)}]
+     [:section.modal-card-body {:class body-class}]
      (for [b body]
        b))
     (when footer
       (into
-       [:footer {:class (cs "modal-card-foot" footer-class)}]
+       [:footer.modal-card-foot {:class footer-class}]
        (for [f footer]
          f)))]
-   [:button {:class (cs "modal-close" "is-large" close-class)
-             :aria-label "close"
-             :on-click #(session/assoc-in! (session-path id) false)}]])
+   [:button.modal-close.is-large {:class close-class
+                                  :aria-label "close"
+                                  :on-click #(session/assoc-in! (session-path id) false)}]])
