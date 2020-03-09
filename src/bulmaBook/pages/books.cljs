705a8a64793dd224a9e96d8d4b4420e539bac95d
Tim Cross <theophilusx@gmail.com>
2020-03-09 16:13:22 +1100

    Add delete book functionality

--- src/bulmaBook/pages/books.cljs
+++ src/bulmaBook/pages/books.cljs
@@ -7,42 +7,85 @@
             [reagent.core :as r]
             [clojure.string :as string]))
 
+(def book-list (r/atom {}))
+
+(defn get-subpage []
+  (store/get-in store/global-state [:ui :books :page]))
+
+(defn set-subpage [page]
+  (store/assoc-in! store/global-state [:ui :books :page] page))
+
+(defn set-edit-id [bid]
+  (store/assoc-in! store/global-state [:ui :books :edit] bid))
+
+(defn get-edit-id []
+  (store/get-in store/global-state [:ui :books :edit]))
+
+(defn set-delete-id [bid]
+  (store/assoc-in! store/global-state [:ui :books :delete] bid))
+
+(defn get-delete-id []
+  (store/get-in store/global-state [:ui :books :delete]))
+
+(defn get-sort-field []
+  (store/get-in store/global-state [:ui :books :sort]))
+
+(defn book-data []
+  (store/get-in store/global-state [:data :book-data]))
+
 (defn books->vec []
-  (let [books (store/get-in store/global-state [:data :book-data])]
+  (let [books (book-data)]
     (mapv #(% books) (keys books))))
 
 (defn gen-new-key []
-  (keyword (str "bk" (inc (count (keys (store/get-in
-                                        store/global-state
-                                        [:data :book-data])))))))
+  (keyword (str "bk" (inc (count (keys (book-data)))))))
 
 (defn get-book [bid]
-  (store/get-in store/global-state [:data :book-data bid]))
+  (bid (book-data)))
 
-(def book-list (r/atom (books->vec)))
+(defn add-book [book]
+  (store/assoc-in! store/global-state [:data :book-data (:id book)] book))
+
+(defn delete-book [bid]
+  (store/update-in! store/global-state [:data :book-data] dissoc bid))
+
+
+(defn filter-books [search-term]
+  (store/reset! book-list (filterv
+                           (fn [bk]
+                             (or (string/includes? (str (:title bk)) search-term)
+                                 (string/includes? (str (:cost bk)) search-term)
+                                 (string/includes? (str (:pages bk)) search-term)
+                                 (string/includes? (str (:isbn bk)) search-term)))
+                           (books->vec))))
+
 
 (defn save-new-book [book]
   (let [new-id (gen-new-key)]
+    (println (str "New Book: " @book))
     (store/put! book :id new-id)
-    (store/update-in! store/global-state [:data :book-data new-id] conj @book)
+    (println (str "New book w/ ID: " @book))
+    (add-book @book)
     (store/clear! book)
-    (store/assoc-in! store/global-state [:ui :books :page] :books)))
+    (set-subpage :books)))
 
 (defn clear-new-book [book]
   (store/clear! book)
-  (store/assoc-in! store/global-state [:ui :books :page] :books))
+  (set-subpage :books))
 
 (defn new-book-form []
   (let [doc (r/atom {})]
     (fn []
       [:form.box
-       [inputs/horizontal-field "Title" [[inputs/input :text :title :modal doc]]]
-       [inputs/horizontal-field "Image" [[inputs/input :text :image :modal doc]]]
-       [inputs/horizontal-field "Cost" [[inputs/input :text :cost :modal doc]]]
-       [inputs/horizontal-field "Pages" [[inputs/input :text :pages :modal doc]]]
-       [inputs/horizontal-field "ISBN" [[inputs/input :text :isbn :modal doc]]]
-       [inputs/button "Save" #(save-new-book doc)
-        :classes {:button "is-success"}]])))
+       [inputs/horizontal-field "Title" [[inputs/input :text :title :model doc]]]
+       [inputs/horizontal-field "Image" [[inputs/input :text :image :model doc]]]
+       [inputs/horizontal-field "Cost" [[inputs/input :text :cost :model doc]]]
+       [inputs/horizontal-field "Pages" [[inputs/input :text :pages :model doc]]]
+       [inputs/horizontal-field "ISBN" [[inputs/input :text :isbn :model doc]]]
+       [inputs/field [[inputs/button "Save" #(save-new-book doc)
+                       :classes {:button "is-success"}]
+                      [inputs/button "Cancel" #(clear-new-book doc)]]
+        :classes {:field "has-addons"}]])))
 
 (defn new-book-page []
   [:<>
@@ -56,19 +99,19 @@
    [new-book-form]])
 
 (defn do-edit-book [bid]
-  (store/assoc-in! store/global-state [:ui :books :edit] bid)
-  (store/assoc-in! store/global-state [:ui :books :page] :edit-book))
+  (set-edit-id bid)
+  (set-subpage :edit-book))
 
 (defn save-edit-book [book]
-  (store/assoc-in! store/global-state [:data :book-data (:id @book)] @book)
+  (add-book @book)
   (store/clear! book)
-  (store/assoc-in! store/global-state [:ui :books :edit] nil)
-  (store/assoc-in! store/global-state [:ui :books :page] :books))
+  (set-edit-id nil)
+  (set-subpage :books))
 
 (defn cancel-edit-book [book]
   (store/clear! book)
-  (store/assoc-in! store/global-state [:ui :books :edit] nil)
-  (store/assoc-in! store/global-state [:ui :books :page] :books))
+  (set-edit-id nil)
+  (set-subpage :books))
 
 (defn edit-book-form []
   (let [doc (r/atom (get-book (store/get-in store/global-state
@@ -97,14 +140,38 @@
       :active true}]]
    [edit-book-form bid]])
 
-(defn filter-books [search-term]
-  (store/reset! book-list (filterv
-                           (fn [bk]
-                             (or (string/includes? (str (:title bk)) search-term)
-                                 (string/includes? (str (:cost bk)) search-term)
-                                 (string/includes? (str (:pages bk)) search-term)
-                                 (string/includes? (str (:isbn bk)) search-term)))
-                           (books->vec))))
+(defn do-delete-book [bid]
+  (set-delete-id bid)
+  (set-subpage :delete-book))
+
+(defn delete-book-page []
+  (let [book (get-book (get-delete-id))]
+    [:<>
+     [breadcrumbs :ui:books:page
+      [{:name "Books"
+        :value :books
+        :active false}
+       {:name "Delete Book"
+        :value :delete-book
+        :active true}]]
+     [:table.table
+      [:tbody
+       [:tr [:th "Title"] [:td (:title book)]]
+       [:tr [:th "Cover"] [:td [:img {:src (:image book) :width "80"}]]]
+       [:tr [:th "Cost"] [:td (:cost book)]]
+       [:tr [:th "Pages"] [:td (:pages book)]]
+       [:tr [:th "ISBN"] [:td (:isbn book)]]]]
+     [inputs/field [[inputs/button "Delete Book"
+                     (fn []
+                       (delete-book (get-delete-id))
+                       (set-delete-id nil)
+                       (set-subpage :books))
+                     :classes {:button "is-success"}]
+                    [inputs/button "Cancel"
+                     (fn []
+                       (set-delete-id nil)
+                       (set-subpage :books))]]
+      :classes {:field "has-addons"}]]))
 
 (defn get-toolbar-data []
   {:left-items [(deftoolbar-item
@@ -115,9 +182,7 @@
                 (deftoolbar-item
                   :type :div
                   :content
-                  [inputs/button "New" #(store/assoc-in!
-                                       store/global-state
-                                       [:ui :books :page] :new-book)
+                  [inputs/button "New" #(set-subpage :new-book)
                    :classes {:button "is-success"}])
                 (deftoolbar-item
                   :class "is-hidden-table-only"
@@ -126,7 +191,7 @@
    :right-items [(deftoolbar-item
                    :content "Order by")
                  (deftoolbar-item
-                   :content [inputs/select-field :data.sort
+                   :content [inputs/select-field :ui.books.sort
                              [[inputs/option "Title" :title]
                               [inputs/option "Price" :cost]
                               [inputs/option "Page Count" :pages]
@@ -146,7 +211,8 @@
                       [:a {:href "#"
                            :on-click #(do-edit-book (:id book))} "Edit"]
                       [:span "·"]
-                      [:a {:href "#"} "Delete"]]]}
+                      [:a {:href "#"
+                           :on-click #(do-delete-book (:id book))} "Delete"]]]}
     :left {:content [[:img {:src (:image book) :width "80"}]]}]])
 
 (defn book-grid-component [books]
@@ -155,13 +221,11 @@
    (for [b books]
      [book-component b])))
 
-(defn books-page []
+(defn books-list-page []
   (reset! book-list (books->vec))
   (fn []
-    (when (store/get-in store/global-state [:data :sort])
-      (reset! book-list (vec (sort-by (store/get-in store/global-state
-                                                    [:data :sort])
-                                      (books->vec)))))
+    (when (get-sort-field)
+      (reset! book-list (vec (sort-by (get-sort-field) (books->vec)))))
     [:div
      [breadcrumbs :ui.books.page
       [{:name "Books"
@@ -169,3 +233,15 @@
         :active true}]]
      [toolbar (get-toolbar-data)]
      [paginate @book-list book-grid-component :page-size 2]]))
+
+(defn books-page []
+  (case (get-subpage)
+    :books [books-list-page]
+    :new-book [new-book-page]
+    :edit-book [edit-book-page]
+    :delete-book [delete-book-page]
+    [:<>
+     [:h2.title "Page Not Found!"]
+     [:p "Bad page identifier " (str (get-subpage))]]))
+
+(set-subpage :books)
