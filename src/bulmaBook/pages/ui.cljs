a91de49f3be59119187dec3a22586a2645a88049
Tim Cross <theophilusx@gmail.com>
2020-03-17 05:10:40 +1100

    Move UI related functions into own namespace

---
+++ src/bulmaBook/pages/ui.cljs
@@ -0,0 +1,21 @@
+(ns bulmaBook.pages.ui
+  (:require [bulmaBook.store :as store]))
+
+(defn set-subpage [group page]
+  (store/assoc-in! store/global-state [:ui group :page] page))
+
+(defn get-subpage [group]
+  (store/get-in store/global-state [:ui group :page]))
+
+(defn set-target [group target]
+  (store/assoc-in! store/global-state [:ui group :target] target))
+
+(defn get-target [group]
+  (store/get-in store/global-state [:ui group :target]))
+
+(defn set-listing-type [group type]
+  (store/assoc-in! store/global-state [:ui group :listing] type))
+
+(defn get-listing-type [group]
+  (store/get-in store/global-state [:ui group :listing]))
+
