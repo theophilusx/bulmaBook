c660c06777ac5ca0f51e174d886115ddd34a6cef
Tim Cross <theophilusx@gmail.com>
2020-03-17 05:11:40 +1100

    Add a get-new-id function and alias for global state to models namespace

--- src/bulmaBook/models.cljs
+++ src/bulmaBook/models.cljs
@@ -1,6 +1,20 @@
 (ns bulmaBook.models
   (:require [bulmaBook.store :as store]))
 
+(def state store/global-state)
+
+(defn get-new-id [group]
+  (case group
+    :book (let [id (inc (store/get-in state [:data :book-counter]))]
+            (store/update-in! state [:data :book-counter] inc)
+            (keyword (str "bk" id)))
+    :customer (let [id (inc (store/get-in state [:data :customer-counter]))]
+                (store/update-in! state [:data :customer-counter] inc)
+                (keyword (str "cs" id)))
+    :order (let [id (inc (store/get-in state [:data :order-counter]))]
+             (store/update-in! state [:data :order-counter] inc)
+             (keyword (str "OR" id)))))
+
 (defn book-data []
   (store/get-in store/global-state [:data :book-data]))
 
@@ -20,6 +34,7 @@
 (defn customer-data []
   (store/get-in store/global-state [:data :customer-data]))
 
+
 (defn customers->vec []
   (let [customers (customer-data)]
     (mapv #(% customers) (keys customers))))
@@ -32,3 +47,29 @@
 
 (defn add-customer [cus]
   (store/assoc-in! store/global-state [:data :customer-data (:id cus)] cus))
+
+(defn order-data []
+  (store/get-in store/global-state [:data :order-data]))
+
+(defn orders->vec []
+  (let [orders (order-data)]
+    (mapv (fn [oid]
+            (let [order (oid orders)
+                  customer (get-customer (:cid order))]
+              {:id oid
+               :cid (:cid order)
+               :customer (str (:first-name customer) " "
+                              (:last-name customer))
+               :date (:date order)
+               :book-count (count (:books order))
+               :status (:status order)
+               :total (reduce (fn [acc bk]
+                                (+ acc (:cost bk)))
+                              0 (:books order))}))
+          (keys orders))))
+
+(defn get-order [oid]
+  (oid (order-data)))
+
+(defn get-customer-orders [cid]
+  (filterv #(= cid (:cid %)) (orders->vec)))
