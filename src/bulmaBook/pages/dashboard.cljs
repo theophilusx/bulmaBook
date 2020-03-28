1baa31e2f08b9dd51e34fbc0469c393bb69fff55
Tim Cross <theophilusx@gmail.com>
2020-03-29 10:40:07 +1100

    Start dashboard page

--- src/bulmaBook/pages/dashboard.cljs
+++ src/bulmaBook/pages/dashboard.cljs
@@ -1,33 +1,22 @@
 (ns bulmaBook.pages.dashboard
-  (:require [bulmaBook.components.basic :refer [breadcrumbs render-map]]
-            [bulmaBook.components.inputs :as inputs]
-            [reagent.core :as r]))
-
-(defn input-sample-form []
-  (let [doc (r/atom {})]
-    (fn []
-      [:form.box
-       [inputs/input-field "Text sample" :text :text-sample :model doc]
-       [inputs/input-field "Email sample" :email :email-sample :model doc]
-       [inputs/input-field "Colour sample" :color :color-sample :size 4 :model doc]
-       [inputs/input-field "Date sample" :date :date-sample :size 12 :model doc]
-       [inputs/input-field "Month sample" :month :month-sample :size "10" :model doc]
-       [inputs/number-field  :number-sample :size 2 :maxlength 4 :max 99 :min 0
-        :model doc :label "Number sample" :value 10]
-       [inputs/range-field :range-field-sample 0 100 :step 10 :value 60
-        :model doc :label "Sample range field"]
-       [:hr]
-       [:input {:type "number" :size "2" :maxlength "2" :min "10" :max "99" :value "20"}]
-       [:hr]
-       [render-map @doc]])))
+  (:require [bulmaBook.components.inputs :as inputs]
+            [bulmaBook.store :as store]))
 
 (defn dashboard-page []
   [:<>
-   [breadcrumbs :ui.dashboard.page
-    [{:name "Dashboard"
-      :value :dashboard
-      :active true}]]
-   [:h2 "Default dashboard page"]
-   [inputs/file :data.file :is-boxed true :classes {:file "is-primary"}]
-   [:hr]
-   [input-sample-form]])
+   [:h1.title "Dashboard"]
+   [:div.level
+    [:div.level-left
+     [:h1.subtitle.is-3
+      [:span.has-text-grey-light "Hello "]
+      [:strong (store/get-in store/global-state [:session :user :name])]]]
+    [:div.leve-right
+     [inputs/select :ui.dashboard.period
+      [(inputs/defoption "Today")
+       (inputs/defoption "Yesterday")
+       (inputs/defoption "This Week" :value "week")
+       (inputs/defoption "This Month" :value "month")
+       (inputs/defoption "This Year" :value "year")
+       (inputs/defoption "All Time" :value "all")]
+      :selected "month" :model store/global-state]]]
+   ])
