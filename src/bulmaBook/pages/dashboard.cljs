8d380556d07a0ea9bd9868e5acbc4242203e5f9d
Tim Cross <theophilusx@gmail.com>
2020-03-29 12:45:46 +1100

    Added first part of dashboard page

--- src/bulmaBook/pages/dashboard.cljs
+++ src/bulmaBook/pages/dashboard.cljs
@@ -1,6 +1,7 @@
 (ns bulmaBook.pages.dashboard
   (:require [bulmaBook.components.inputs :as inputs]
-            [bulmaBook.store :as store]))
+            [bulmaBook.store :as store]
+            [bulmaBook.components.basic :as basic]))
 
 (defn dashboard-page []
   [:<>
@@ -19,4 +20,24 @@
        (inputs/defoption "This Year" :value "year")
        (inputs/defoption "All Time" :value "all")]
       :selected "month" :model store/global-state]]]
-   ])
+   [:div.columns.is-multiline
+    [:div.column.is-12-tablet.is-6-desktop.is-3-widescreen
+     [basic/notification [:<>
+                          [:p.title.is-1 "232"]
+                          [:p.subtitle.is-4 "Orders"]]
+      :class "is-link has-text"]]
+    [:div.column.is-12-tablet.is-6-desktop.is-3-widescreen
+     [basic/notification [:<>
+                          [:p.title.is-1 "$7,648"]
+                          [:p.subtitle.is-4 "Revenue"]]
+      :class "is-info has-text"]]
+    [:div.column.is-12-tablet.is-6-desktop.is-3-widescreen
+     [basic/notification [:<>
+                          [:p.title.is-1 "1,678"]
+                          [:p.subtitle.is-4 "Visitors"]]
+      :class "is-primary has-text"]]
+    [:div.column.is-12-tablet.is-6-desktop.is-3-widescreen
+     [basic/notification [:<>
+                          [:p.title.is-1 "20,756"]
+                          [:p.subtitle.is-4 "Pages"]]
+      :class "is-success has-text"]]]])
