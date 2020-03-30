76cb11842492f032ad116de82317e5ebe452b50b
Tim Cross <theophilusx@gmail.com>
2020-03-30 13:54:53 +1100

    Added dashboard data to global state

--- src/bulmaBook/core.cljs
+++ src/bulmaBook/core.cljs
@@ -30,6 +30,7 @@
 (store/assoc-in! store/global-state [:data :customer-counter] data/customer-counter)
 (store/assoc-in! store/global-state [:data :order-data] data/order-data)
 (store/assoc-in! store/global-state [:data :order-counter] data/order-counter)
+(store/assoc-in! store/global-state [:data :dashboard-data] data/dashboard-data)
 (mount-app)
 
 ;; specify reload hook with ^;after-load metadata
